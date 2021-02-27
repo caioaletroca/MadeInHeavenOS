325903d1724f81625a5521347063c478168a7e33
Caio Alexandre Troti Caetano <caio.caetano@dock.tech>
2021-02-27 18:11:41 -0300

    64 boot mode

--- kernel/arch/x86_64/boot.asm
+++ kernel/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global _start
+extern long_mode_start
 
 section .text
 bits 32
@@ -19,8 +20,71 @@ _start:
 	; in assembly as languages such as C cannot function without a stack.
 	mov esp, stack_top
 
-	mov dword [0xb8000], 0x2f4b2f4f
+	; call check_multiboot
 
+	call setup_page_tables
+	call enable_paging
+
+	lgdt [gdt64.pointer]
+	jmp gdt64.code_segment:long_mode_start
+
+	hlt
+
+; check_multiboot:
+; 	cpm eax, 0x36D76289
+; 	jne .no_multiboot
+; 	ret
+; .no_multiboot:
+; 	mov al, "M"
+; 	jmp error
+
+setup_page_tables:
+	mov eax, page_table_l3
+	or eax, 0b11			; Enable Present and R/W flags
+	mov [page_table_l4], eax
+
+	mov eax, page_table_l2
+	or eax, 0b11			; Enable Present and R/W flags
+	mov [page_table_l3], eax
+
+	mov ecx, 0				; Start counter
+
+.loop:
+	mov eax, 0x200000		; 2 MiB data
+	mul ecx
+	or eax, 0b10000011		; Enable Present, R/W flags and huge page
+	mov [page_table_l2 + ecx * 8], eax
+
+	inc ecx
+	cmp ecx, 512			; Check for all memory mapped
+	jne .loop
+
+	ret
+
+enable_paging:
+	mov eax, page_table_l4
+	mov cr3, eax
+
+	; Enable PAE
+	mov eax, cr4
+	or eax, 1 << 5
+	mov cr4, eax
+
+	; Enable long mode
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
+
+	; Enable paging
+	mov eax, cr0
+	or eax, 1 << 31
+	mov cr0, eax
+
+	ret
+
+error:
+	mov byte [0xB8000], al
 	hlt
 
 ; The multiboot standard does not define the value of the stack pointer register
@@ -35,6 +99,21 @@ _start:
 ; undefined behavior.
 section .bss
 	align 4096
+page_table_l4:
+	resb 4096
+page_table_l3:
+	resb 4096
+page_table_l2:
+	resb 4096
 stack_bottom:
 	resb 4096 * 4
 stack_top:
+
+section .rodata
+gdt64:
+	dq 0 ; zero entry
+.code_segment: equ $ - gdt64
+	dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53) 	; code segment
+.pointer:
+	dw $ - gdt64 - 1 									; length
+	dq gdt64 											; address
---
+++ kernel/arch/x86_64/boot64.asm
@@ -0,0 +1,16 @@
+global long_mode_start
+
+section .text
+bits 64
+long_mode_start:
+    ; Load null into all data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov dword [0xb8000], 0x2f4b2f4f
+
+	hlt
