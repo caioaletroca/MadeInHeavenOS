6be4670941b97a70fb7a4e3464e9bc8ca238dc88
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2022-04-09 16:29:17 -0300

    Implemented Screen

---
+++ kernel/driver/screen.c
@@ -0,0 +1,27 @@
+#include "screen.h"
+
+void screen_put(struct screen *scr, char c) {
+    // Insert new character into the buffer
+    scr->buf[scr->pos_y * SCREEN_WIDTH + scr->pos_x] = c;
+
+    // Add to the position
+    scr->pos_x++;
+
+    // Jump to a new line
+    if(scr->pos_x >= SCREEN_WIDTH) {
+        scr->pos_x = 0;
+        scr->pos_y++;
+    }
+
+    // TODO: Scroll the screen
+}
+
+void screen_write(struct screen *scr, const char *str, size_t n) {
+    for(size_t i = 0; i < n; i++)
+        screen_put(scr, str[i]);
+}
+
+void screen_init(struct screen *scr) {
+    memset(scr->buf, ' ', sizeof(scr->buf));
+    scr->pos_x = scr->pos_y = 0;
+}
---
+++ kernel/driver/screen.h
@@ -0,0 +1,41 @@
+#ifndef _SCREEN_H_
+#define _SCREEN_H_
+
+#define SCREEN_WIDTH    80
+#define SCREEN_HEIGHT   25
+
+#include <stddef.h>
+
+struct screen {
+    unsigned int pos_x;
+    unsigned int pos_y;
+    char buf[SCREEN_WIDTH * SCREEN_HEIGHT];
+};
+
+/**
+ * @brief Screen initialization
+ * 
+ * Clears the screen and set the cursor position at the start position
+ * 
+ * @param scr 
+ */
+void screen_init(struct screen *scr);
+
+/**
+ * @brief Puts a single character to the screen
+ * 
+ * @param scr Screen context
+ * @param c Character
+ */
+void screen_put(struct screen *scr, char c);
+
+/**
+ * @brief Write a character string to the screen
+ * 
+ * @param scr Screen context
+ * @param str Characters string
+ * @param n String length
+ */
+void screen_write(struct screen *scr, const char *str, size_t n);
+
+#endif
