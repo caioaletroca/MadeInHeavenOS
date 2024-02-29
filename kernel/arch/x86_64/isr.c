3de98996b8221859372dedca3a17a2361eda7407
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2024-02-28 23:44:29 -0300

    Implemented error code capture

--- kernel/arch/x86_64/isr.c
+++ kernel/arch/x86_64/isr.c
@@ -1,6 +1,29 @@
 #include "isr.h"
 #include <kprintf.h>
 
-void isr_handler(isr_context isr_reg) {
-    kprintf("Interrupt!!!");
+void isr_handler(isr_context *regs) {
+    uint8_t int_no = (regs->info >> 32) & 0xFF;
+    uint8_t err_code = regs->info & 0xFF;
+    
+    kprintf("int_no: %i\n", int_no);
+    kprintf("err_code: %i\n", err_code);
+
+    // kprintf("rax: %i\n", regs->rax);
+    // kprintf("rdi: %i\n", regs->rdi);
+    // kprintf("rsi: %i\n", regs->rsi);
+    // kprintf("rcx: %i\n", regs->rcx);
+    // kprintf("r8: %i\n", regs->r8);
+    // kprintf("r9: %i\n", regs->r9);
+    // kprintf("r10: %i\n", regs->r10);
+    // kprintf("r11: %i\n", regs->r11);
+    
+    // kprintf("info: %i\n", regs->info);
+
+    // kprintf("rip: %i\n", regs->rip);
+    // kprintf("cs: %i\n", regs->cs);
+    // kprintf("rflags: %i\n", regs->rflags);
+    // kprintf("rsp: %i\n", regs->rsp);
+    // kprintf("ss: %i\n", regs->ss);
+
+    __asm__ __volatile__ ("xchgw %bx, %bx");
 }
--- kernel/include/isr.h
+++ kernel/include/isr.h
@@ -4,17 +4,26 @@
 #include <stdint.h>
 
 typedef struct {
-    uint64_t r11;
-    uint64_t r10;
-    uint64_t r9;
-    uint64_t r8;
-    uint64_t rcx;
-    uint64_t rdx;
-    uint64_t rsi;
-    uint64_t rdi;
     uint64_t rax;
+    uint64_t rdi;
+    uint64_t rsi;
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t r8;
+    uint64_t r9;
+    uint64_t r10;
+    uint64_t r11;
+
+    uint64_t info;
+
+    // Interrupt stack frame
+	uint64_t rip;
+	uint64_t cs;
+	uint64_t rflags;
+	uint64_t rsp;
+	uint64_t ss;
 } isr_context;
 
-void isr_handler(isr_context isr_reg);
+void isr_handler(isr_context *regs);
 
 #endif
