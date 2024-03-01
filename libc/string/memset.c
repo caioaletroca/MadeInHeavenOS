78dad84db082979f1b1cf60f0318206f7e75cb7a
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2024-03-01 18:29:14 -0300

    Fixed memset

--- libc/string/memset.c
+++ libc/string/memset.c
@@ -1,14 +1,23 @@
 #include <string.h>
 
+/**
+ * Citating a good person:
+ * 
+ * "GCC is very good at optimizing. It recognizes that your for loop is equivalent to memset,
+ * so it replaces the loop with a call to memset. As you've already noticed,
+ * that can be problematic when it happens inside your implementation of memset."
+ * 
+ * The solution was to add the attribute on this function, this may happen in other functions like
+ * memcpy, memmove, memset, and memcmp.
+ * 
+ * Full solution: https://www.reddit.com/r/osdev/comments/1b446qe/gcc_assembled_code_turn_into_a_infinity_loop/
+*/
+__attribute__((optimize("no-tree-loop-distribute-patterns")))
 void *memset(void *str, int c, size_t n) {
     char* buf = (char*) str;
 
     for(size_t i = 0; i < n; i++) {
         buf[i] = c;
-
-        // TODO: DIRT HACK ALERT!!!
-        // Find a way to fix GCC infinity loop without this
-        __asm__ __volatile__ ("nop");
     }
 
     return str;
