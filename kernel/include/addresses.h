cee03beb4d0c377b1f716d30913b3f6d0ca27c31
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2024-05-26 18:19:35 -0300

    More good stuff

---
+++ kernel/include/addresses.h
@@ -0,0 +1,18 @@
+#ifndef _ADDRESSES_H_
+#define _ADDRESSES_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <memory.h>
+
+typedef uintptr_t physaddr_t;
+typedef void *virtaddr_t;
+typedef void *kernaddr_t;
+
+static inline kernaddr_t phys_to_kern(physaddr_t p) {
+	if (p == (physaddr_t)NULL)
+		return NULL;
+	return (kernaddr_t)(p + KERNEL_ADDRESS);
+}
+
+#endif
--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,9 +1,18 @@
+#include <addresses.h>
 #include <kprintf.h>
 #include <interrupts.h>
+#include <multiboot2.h>
 #include "idt.h"
 #include "driver/ps2.h"
 
-void kmain() {
+// #define ALIGNUP(val, align) ({ \
+// 	const typeof((val)) _val = (val); \
+// 	const typeof((align)) _align = (align); \
+// 	(_val + (_align - 1)) & -_align; })
+
+void kmain(physaddr_t address) {
+    struct multiboot_info *info = (struct multiboot_info*)phys_to_kern(address);
+
     kprintf("Alive!!!\n");
 
     interrupts_init();
@@ -12,9 +21,20 @@ void kmain() {
 
     enable_interrupts();
 
+    kprintf("Phys Address: %p\n", address);
+    kprintf("Kernel Address: %p\n", phys_to_kern(address));
+    kprintf("Multiboot Total Size: %X\n", info->total_size);
+    kprintf("Reserved: %p\n", info->reserved);
+    kprintf("First Tag Type: %u\n", info->tags[0].type);
+
+    // struct multiboot_tag* tags = (struct multiboot_tag*)info->tags;
+    // for(; tags->type != 0; tags = (struct multiboot_tag*)ALIGNUP((uintptr_t)tags + tags->size, 8)) {
+    //     kprintf("Tag Type: %u\n", tags->type);
+    // }
+
+    // Magic breakpoint
     __asm__ __volatile__ ("xchgw %bx, %bx");
 
-    // __asm__ __volatile__ ("int $2");
     while(1) {
         __asm__ __volatile__ ("hlt");
     }
